7448cef2dc4c128c222da4b2940ea0db37ba1782
Tejas Gandre <tgandre007@gmail.com>
2020-12-28 15:41:53 -0800

    Completed questions.js

--- My Testing/questions.js
+++ My Testing/questions.js
@@ -16,17 +16,25 @@ const ask = (i = 0) => {
 // calling the function
 ask();
 
+
+// create an array to store the answers
+const answers = [];
+
 // wireup a listener using standard input
 // first argument is the data event that we are listening for
 // i.e. we are reading input from keyboard
 // the second argument is the handler or the function that we will use to handle that event
 process.stdin.on('data', data => {
-    //echoing data backk to the user
-    // the data is passed to the function as a buffer or a binary
-    // hence it needs to be converted to string
-    process.stdout.write(`\n\n ${data.toString().trim()}\n\n`);
-    // exit using the following command and avoid infinite loop.
-    process.exit();
+    //storing all the answers in the answers array
+    answers.push(data.toString().trim());
+
+    //check the length of the answers array
+    if(answers.length < questions.length){
+        // if condition is satisfied then we are asking the next question
+        ask(answers.length);
+    } else{
+        process.exit();
+    }
 });
 
 // The above code ends in ainfinite loop.
