7ed6f4b8e0a145786204347c6af91ef06c2e2ec8
Tejas Gandre <tgandre007@gmail.com>
2020-12-28 17:40:05 -0800

    added progress report and other functionalities

--- My Testing/timers.js
+++ My Testing/timers.js
@@ -1,16 +1,24 @@
 // working with node.js asnchronously using timing functions
 // like setTimeout, clearTimeout, setInterval, and clearInterval
 
-// creating 3sec delay using setTimeout
+// we will now communicate with waiting time in % to show progress
 
-const waitTime = 3000;
+// creating 5sec delay using setTimeout
+
+const waitTime = 5000;
 const waitInterval = 500;
 // let lets you change the value of the variables unlike const
 let currentTime = 0;
 
 const incTime = () => {
-    currentTime += waitInterval;;
-    console.log(`waiting ${currentTime/1000} seconds \n`);
+    currentTime += waitInterval;
+    const p = Math.floor((currentTime/waitTime) * 100);
+    // clear the last line written in the terminal
+    process.stdout.clearLine();
+    // move the cursor to the start position
+    process.stdout.cursorTo(0);
+    // write the message
+    process.stdout.write(`waiting ... ${p}%`);
 }
 
 console.log(`Setting a ${waitTime / 1000} seconds`);
@@ -18,8 +26,13 @@ console.log(`Setting a ${waitTime / 1000} seconds`);
 // invoking a function after the timer is finished
 // int the following code, we are clearing the intercal as well.
 const timerFinished = () => {
+    // clear the last line written in the terminal
+    process.stdout.clearLine();
+    // move the cursor to the start position
+    process.stdout.cursorTo(0);
+    // clear the interval
     clearInterval(interval);
-    console.log("done");
+    console.log("Done");
     // the apploication exits because the interval is cleared and no longer ticking
 }
 
