4e598f2e1305376904d5537d2e76800026761cc4
Tejas Gandre <tgandre007@gmail.com>
2020-12-28 18:29:09 -0800

    destructuring util and v8 modules

--- My Testing/core.js
+++ My Testing/core.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const util = require("util");
-const v8 = require("v8");
+const {log} = require("util");
+const {getHeapCodeStatistics} = require("v8");
 
 // with utilit module we also get the date when we do util.log
-util.log(v8.getHeapCodeStatistics());
+log(getHeapCodeStatistics());
